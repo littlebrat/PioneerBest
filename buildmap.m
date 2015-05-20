ff974e8585a75473e06f9d19047f7cff85607db1
littlebrat <nuno.laurentino.mendes@ist.utl.pt>
2015-05-20 11:02:05 +0100

    buildmap added

---
+++ buildmap.m
@@ -0,0 +1,15 @@
+function result = buildmap(map,odom,sonar,thr)
+    mypoints = [71 133+sonar(1);165+sonar(2) 26; 165+sonar(3) -26; 71 -133-sonar(4)];
+    x=odom(1);
+    y=odom(2);
+    tet=odom(3);
+     
+    for i=1:4
+       mypoints(i,1)= x + P(i,1)*cos(tet)+P(i,2)*sin(tet);
+       mypoints(i,2)= y - P(i,1)*sin(tet)+P(i,2)*cos(tet);
+       if abs(mypoints(i,1)-x)<thr && abs(mypoints(i,2)-y)<thr
+         map=[map; P(i,:)];
+       end
+    end
+    result=map;
+end
