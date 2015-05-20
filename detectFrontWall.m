068568c1adbeea8d59e5c82de2b54652a353822a
littlebrat <nuno.laurentino.mendes@ist.utl.pt>
2015-05-20 12:21:49 +0100

    update

---
+++ detectFrontWall.m
@@ -0,0 +1,17 @@
+function result = detectFrontWall(map,odom,thrpoints)
+    x=odom(1);
+    y=odom(2);
+    pointsdetected=0;
+    mydistance=0;
+    for i=0:size(map,1)
+        if map(i,1)-x<5000  && abs(map(i,2)-x)< 400 && map(i,1)>0
+            mydistance=mydistance+map(i,1);%nao tenho a certeza se é x ou y
+        end
+    end
+    if pointsdetected>thrpoints
+        result=mydistance/pointsdetected;
+    else
+        result=10000;
+    end
+    
+end
